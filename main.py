9d800b3d9ab5c5d4f2e0b366dd558d3f79fd485d
jacobhsu <jacob.hsu.tw@gmail.com>
2023-04-27 16:28:39 +0800

    feat: schedule.every().day

--- main.py
+++ main.py
@@ -2,38 +2,45 @@ import yfinance as yf
 import datetime as dt
 import requests
 import os
+import schedule
+import time
 
-# 获取台湾大盘指数的数据
-twii = yf.Ticker("^TWII")
+def main():
+    # 获取台湾大盘指数的数据
+    twii = yf.Ticker("^TWII")
 
-# 获取当日数据
-data = twii.history(period="1d")
+    # 获取当日数据
+    data = twii.history(period="1d")
 
-# 获取当日收盘价，并将其转换为整数
-close_price = int(data["Close"][0])
+    # 获取当日收盘价，并将其转换为整数
+    close_price = int(data["Close"][0])
 
-# 使用os.environ获取Github仓库的secrets
-tg_api_token = os.environ.get('TELEGRAM_API_TOKEN')
-tg_chat_id = os.environ.get('CHAT_ID')
+    # 使用os.environ获取Github仓库的secrets
+    tg_api_token = os.environ.get('TELEGRAM_API_TOKEN')
+    tg_chat_id = os.environ.get('CHAT_ID')
 
+    # 打印当日收盘价
+    print("当日收盘价：", close_price)
 
-# 打印当日收盘价
-print("当日收盘价：", close_price)
-print("dotenv CHAT_ID：", tg_chat_id)
-print("yml API_TOKEN", tg_api_token)
+    # 發電報函數
+    def send_to_telegram(message):
+        apiToken = tg_api_token
+        chatID = tg_chat_id
+        apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+        try:
+            response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+            print(response.text)
+        except Exception as e:
+            print(e)
 
-# 發電報函數
-def send_to_telegram(message):
-    apiToken = tg_api_token
-    chatID = tg_chat_id
-    apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
-    try:
-        response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
-        print(response.text)
-    except Exception as e:
-        print(e)
+    # 發送消息至telegram
+    current_time = dt.datetime.now().strftime("%Y-%m-%d")
+    message = f"日期:{current_time} 當日收盤價:{close_price}"
+    send_to_telegram(message)
 
-# 發送消息至telegram
-current_time = dt.datetime.now().strftime("%Y-%m-%d")
-message = f"現在時間:{current_time}當日收盤價:{close_price}"
-send_to_telegram(message)
+schedule.every().day.at('17:00').do(main)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
