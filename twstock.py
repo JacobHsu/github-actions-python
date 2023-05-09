ad8e4ff54b8818f6fa960519c9353ebdaaabbbd5
jacobhsu <jacob.hsu.tw@gmail.com>
2023-05-09 18:44:30 +0800

    twstock

---
+++ twstock.py
@@ -0,0 +1,35 @@
+import twstock
+import requests
+import os
+
+def main():
+
+    def best_four_point_to_buy(stockid):
+        stock = twstock.Stock(stockid)
+        bp = twstock.BestFourPoint(stock).best_four_point()
+        if bp:
+            print('買進訊號', stockid)
+            print(bp)
+            # 發送消息至telegram
+            message = f"買進訊號:{stockid}\n{bp}"
+            send_to_telegram(message)
+        else:
+            print('無買進訊號', stockid)
+
+
+    # 發電報函數
+    def send_to_telegram(message):
+        apiToken = os.environ.get('TELEGRAM_API_TOKEN')
+        chatID = os.environ.get('CHAT_ID')
+        apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+        try:
+            response = requests.post(apiURL, json={'chat_id': chatID, 'text': message})
+            print(response.text)
+        except Exception as e:
+            print(e)
+
+    # 發送消息至telegram
+    for stockid in ['1440','2002','2014','2334','2614']:
+        best_four_point_to_buy(stockid)
+
+main()
