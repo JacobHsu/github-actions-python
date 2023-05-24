9ebe1cf4eee461e0dc1d0b5c084c1d14c30b5c2c
jacobhsu <jacob.hsu.tw@gmail.com>
2023-05-24 23:54:42 +0800

    feat: add helpter

--- helper.py
+++ helper.py
@@ -1,14 +1,7 @@
-import os
 import requests
-import datetime as dt
-import tabulate
+import os
 
-# 發電報函數
-def send_to_telegram(codes_and_prices, url):
-    # 發送消息至telegram
-    table = tabulate.tabulate(codes_and_prices, tablefmt='simple')
-    current_time = dt.datetime.now().strftime("%Y-%m-%d")
-    message = f"日期:{current_time} [高股息買入]({url}):\n{table}"
+def send_to_telegram(message):
     # 使用os.environ获取Github仓库的secrets
     apiToken = os.environ.get('TELEGRAM_API_TOKEN')
     chatID = os.environ.get('CHAT_ID')
