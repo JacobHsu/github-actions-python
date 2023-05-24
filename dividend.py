2f2cf7fb557da0e410bf0447f9fdd154e4837afc
jacobhsu <jacob.hsu.tw@gmail.com>
2023-05-24 23:18:20 +0800

    fix: dividend

--- dividend.py
+++ dividend.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-
+import datetime as dt
+import os
+import tabulate
 import re
-import helper
 
 def main():
     url = 'https://tw.tradingview.com/markets/stocks-taiwan/market-movers-high-dividend/'
@@ -36,6 +37,23 @@ def main():
 
     # Print the list of codes and prices
     print(codes_and_prices)
-    helper.send_to_telegram(codes_and_prices, url)
+
+    # 發電報函數
+    def send_to_telegram(message):
+        # 使用os.environ获取Github仓库的secrets
+        apiToken = os.environ.get('TELEGRAM_API_TOKEN')
+        chatID = os.environ.get('CHAT_ID')
+        apiURL = f'https://api.telegram.org/bot{apiToken}/sendMessage'
+        try:
+            response = requests.post(apiURL, data={'chat_id': chatID, 'text': message, 'parse_mode': 'Markdown'})
+            print(response.text)
+        except Exception as e:
+            print(e)
+
+    # 發送消息至telegram
+    table = tabulate.tabulate(codes_and_prices, tablefmt='simple')
+    current_time = dt.datetime.now().strftime("%Y-%m-%d")
+    message = f"日期:{current_time} [高股息買入]({url}):\n{table}"
+    send_to_telegram(message)
 
 main()
