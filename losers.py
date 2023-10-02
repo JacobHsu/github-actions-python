40bd51cfc3ad580df3edd6879aeb389425a6b94a
JacobHsu <jacob.hsu.tw@gmail.com>
2023-10-02 18:02:46 +0800

    Update losers.py

--- losers.py
+++ losers.py
@@ -30,10 +30,9 @@ def main():
             # Extract the data from the cells
             code = cells[0].text.strip()[1:] 
             price = cells[2].text.strip()
-            rating = cells[4].text.strip()
-            ttm = cells[9].text.strip()[0]
+            rating = cells[10].text.strip()
             # Check if the rating is "買入"
-            if rating == "買入" and ttm != "−":
+            if rating == "買入":
                 stock = re.search(r'\d+', code).group()
                 codes_and_prices.append((code, price)) if stock not in my_remove_list else None
 
